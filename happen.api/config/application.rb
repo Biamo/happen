460cac546a8371843754018f8ce9dce2678db7ca
amir bidva <a.bidva@gmail.com>
2017-07-16 23:52:09 -0400

    fix application running libs classes issue

--- happen.api/config/application.rb
+++ happen.api/config/application.rb
@@ -27,6 +27,6 @@ module HappenApi
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.middleware.use ActionDispatch::Flash
-    config.autoload_paths << rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
